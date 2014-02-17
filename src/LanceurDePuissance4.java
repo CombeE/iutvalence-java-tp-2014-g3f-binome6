be7ccbf03e43ac87cb67eef761a33e3feda3b284
ramilijn <nathan.ramilijaona@iut-valence.fr>
2014-02-17 16:38:18 +0100

    Le lanceur de partie de puissance4 et la classe puissance4 qui représente une partie de puissance4

---
+++ src/LanceurDePuissance4.java
@@ -0,0 +1,16 @@
+
+public class LanceurDePuissance4 {
+
+	public static void main(String[] args) {
+		System.out.println ("Création d'une partie de Puissance 4");
+		
+		Puissance4 PartieDePuissance4 = new Puissance4();
+		
+		System.out.println("Demarrage de la partie");
+		
+		PartieDePuissance4.jouer();
+		
+		System.out.println("Fin de la partie");
+	}
+
+}
---
+++ src/Puissance4.java
@@ -0,0 +1,17 @@
+
+
+/**
+ *Une partie de puissance4
+ */
+public class Puissance4 {
+
+	/**
+	 *Joue l'intégralité d'une partie
+	 * 
+	 */
+	public void jouer(){
+		
+		System.out.println("Partie démarrée");
+		
+	}
+}
