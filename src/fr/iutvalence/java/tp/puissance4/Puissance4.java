61e8539c569b0c443bf19341bd6aad53fcc93931
ramilijn <nathan.ramilijaona@iut-valence.fr>
2014-02-24 16:41:01 +0100

    On a enlevé les constantes inutiles : jetons_joueurs et couleur_jetons

--- src/fr/iutvalence/java/tp/puissance4/Puissance4.java
+++ src/fr/iutvalence/java/tp/puissance4/Puissance4.java
@@ -14,11 +14,8 @@ public class Puissance4
 	 */
 	public final static int CAPACITE_PAR_DEFAUT = 42;
 	
-	// TODO (fait) corriger le commentaire 
-	/**
-	 * Le nombre de jetons qu'un joueur a en début de partie
-	 */
-	public final static int JETONS_PAR_DEFAUT = 21;
+	
+	
 
 	// TODO(fait) corriger le commentaire 
 	/**
@@ -26,46 +23,17 @@ public class Puissance4
 	 */
 	
 
-	public int nombreJetonsJoues;
+	private int nombreJetonsJoues;
 	
-	/**
-	 *Valeur pour la couleur rouge 
-	 */
-	
-	public final static int COULEUR_JETON_ROUGE = 1;
-	
-	/**
-	 *Valeur pour la couleur jaune
-	 */
-	
-	public final static int COULEUR_JETON_JAUNE = 2;
-	
+
 
 	// TODO(fait) corriger le commentaire 
 	/**
 	 * La capacité de la grille de puissance 4
 	 */
-	public int capaciteGrille;
+	private int capaciteGrille;
 	
-	/**
-	 * Le nombre de jeton du joueur 1
-	 */
-	public int jetonsJoueur1;
-	
-	/**
-	 * Le nombre de jeton du joueur 2
-	 */
-	public int jetonsJoueur2;
 
-	/**
-	 * la couleur des jetons du joueur 1
-	 */
-	public int couleurJetons1;
-
-	/**
-	 * la couleur des jetons du joueur 2
-	 */
-	public int couleurJetons2;
 
 	// TODO (fait) corriger le commentaire 
 	// TODO(fait) Ecrire un commentaire plus précis
@@ -75,11 +43,6 @@ public class Puissance4
 	public Puissance4()
 	{
 		this.capaciteGrille = CAPACITE_PAR_DEFAUT;
-		this.jetonsJoueur1 = JETONS_PAR_DEFAUT;
-		this.jetonsJoueur2 = JETONS_PAR_DEFAUT;
-		// TODO(fait) Définir des constantes pour les couleurs
-		this.couleurJetons1 = COULEUR_JETON_ROUGE;
-		this.couleurJetons2 = COULEUR_JETON_JAUNE;
 		this.nombreJetonsJoues = 0;
 	}
 	
@@ -91,6 +54,7 @@ public class Puissance4
 	public void jouer()
 	{
 		// TODO(fait) corriger les accents
+		
 		System.out.println("Partie démarrée");
 	}
 }
