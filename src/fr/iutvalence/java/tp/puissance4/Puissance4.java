bb45bf69b1784a47ed6279d4297e85d1bcbe73dd
ramilijn <nathan.ramilijaona@iut-valence.fr>
2014-03-10 17:16:19 +0100

    On a créé la fonction getColonne ainsi que les constantes qu'elle utilise .

--- src/fr/iutvalence/java/tp/puissance4/Puissance4.java
+++ src/fr/iutvalence/java/tp/puissance4/Puissance4.java
@@ -1,5 +1,7 @@
 package fr.iutvalence.java.tp.puissance4;
 
+import java.util.Scanner;
+
 // TODO(fait) Ecrire un commentaire plus précis
 /**
  * Les caractéristiques d'une partie de puissance4.
@@ -13,7 +15,8 @@ public class Puissance4
 	 * Le nombre d'emplacement total dans la grille
 	 */
 	public final static int CAPACITE_PAR_DEFAUT = 42;
-	
+
+
 	
 	
 
@@ -25,8 +28,21 @@ public class Puissance4
 
 	private int nombreJetonsJoues;
 	
-
-
+	/**
+	 * Indicateur permettant de savoir si une colonne est pleine
+	 */
+	private boolean colonnePleine=true;
+	
+	/**
+	 *Une colonne que l'utilisateur utilisera pour placer son jeton 
+	 */
+	private int Colonne;
+	
+	/**
+	 * Ligne permettant de parcourir la colonne et de savoir ou l'on peut écrire
+	 */
+	private int Ligne=5;
+	
 	// TODO(fait) corriger le commentaire 
 	/**
 	 * La capacité de la grille de puissance 4
@@ -55,20 +71,41 @@ public class Puissance4
 			}
 		}
 	}
-	
+
 	/**
 	 * Demande une colonne de la grille sur laquelle on va poser notre jeton
-	 * @param colonne ou l'on pose notre jeton
+	 * et stocke le jeton dans la colonne choisie mais que dans la dernière ligne
 	 */
-	public void getColonne(int colonne){
-		
-		
+	
+	public void getColonne(){
+		while(colonnePleine=true){
+			Scanner choixColonne= new Scanner(System.in);
+			System.out.println("Entrez la colonne dans laquelle vous jouez:");
+			int colonne = choixColonne.nextInt();// L'utilisateur choisi la colonne dans laquelle il joue
+			Colonne = colonne;
+			if (this.grille[0][Colonne]!=0){
+				colonnePleine=true;
+				System.out.println("Colonne pleine, choississez une autre colonne.");
+				}
+			else
+				colonnePleine=false;
+		}
+		while(this.grille[Ligne][Colonne]==0){
+				Ligne--;
+		}
+		if(this.nombreJetonsJoues%2 !=0) // Permet de savoir qui est le joueur
+				this.grille[Ligne][Colonne]=1;
+		else
+				this.grille[Ligne][Colonne]=2;
+
 		
 	}
+
 	// TODO(fait) déplacer les méthodes après les constructeurs
 	// TODO(fait) corriger le commentaire 
 	/**
 	 * Joue l'intégralité d'une partie.
+	 * Il reste à régler le fait qu'un joueur ait gagné
 	 */
 	public void jouer()
 	{
@@ -77,7 +114,14 @@ public class Puissance4
 		System.out.println("Partie démarrée");
 		System.out.println("Joueur 1 : Jetons rouge");
 		System.out.println("Joueur 2 : Jetons jaune");
-
+		while(this.nombreJetonsJoues < this.capaciteGrille){
+			System.out.println("JOUEUR 1"); // fait jouer le joueur 1 
+			getColonne();
+			this.nombreJetonsJoues++; 
+			System.out.println("JOUEUR 2");
+			getColonne();
+			this.nombreJetonsJoues++;
+		}
 		
 	}
 }
