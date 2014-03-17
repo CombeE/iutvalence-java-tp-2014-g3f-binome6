cdcdd627d38554788afeb156d4bf53ec4eaccd6f
ramilijn <nathan.ramilijaona@iut-valence.fr>
2014-03-17 16:46:11 +0100

    Création de la méthode obtenirColonneOuJouer et utilisation d'un nombre aléatoire

--- src/fr/iutvalence/java/tp/puissance4/Joueur.java
+++ src/fr/iutvalence/java/tp/puissance4/Joueur.java
@@ -1,44 +1,20 @@
 package fr.iutvalence.java.tp.puissance4;
 
-import java.util.Scanner;
-
+/**
+ * Les caractéristiques ainsi que les actions possibles d'un joueur de puissance 4
+ * @author ramilijn
+ *
+ */
 public class Joueur
 {
 	/**
-	 * Demande une colonne de la grille sur laquelle on va poser notre jeton et
-	 * stocke le jeton dans la colonne choisie mais que dans la dernière ligne
+	 * Demande une colonne de la grille sur laquelle on va poser notre jeton 
 	 */
-	// TODO(fait) déporter cette méthode dans une classe représentant le joueur
-	public static void getColonne()
+	
+	public int obtenirColonneOuJouer()
 	{
-		Puissance4.colonnePleine=true;
-		while (Puissance4.colonnePleine)
-		{
-			Scanner choixColonne = new Scanner(System.in);
-			System.out.println("Entrez la colonne dans laquelle vous jouez:");
-			int colonne = choixColonne.nextInt();// L'utilisateur choisi la
-													// colonne dans laquelle il
-													// joue
-			
-			if (Puissance4.grille[0][colonne] != 0)
-			{
-				Puissance4.colonnePleine = true;
-				System.out
-						.println("Colonne pleine, choississez une autre colonne.");
-			}
-			else
-				Puissance4.colonnePleine = false;
-		}
-		while (Puissance4.grille[Puissance4.ligne][Puissance4.colonne] == 0)
-		{
-			Puissance4.ligne--;
-		}
-		if (Puissance4.nombreJetonsJoues % 2 != 0) // Permet de savoir qui est le
-												// joueur
-			Puissance4.grille[Puissance4.ligne][Puissance4.colonne] = 1;
-		else
-			Puissance4.grille[Puissance4.ligne][Puissance4.colonne] = 2;
+		int random = (int)(Math.random() * (7));
+		return random; //On obtient un chiffre aléatoire déterminant la colonne jouée
 		
-
 	}
 }
--- src/fr/iutvalence/java/tp/puissance4/Puissance4.java
+++ src/fr/iutvalence/java/tp/puissance4/Puissance4.java
@@ -11,66 +11,43 @@ public class Puissance4
 	/**
 	 * Le nombre d'emplacements total dans la grille
 	 */
-	public final static int CAPACITE_PAR_DEFAUT = 42;
+	public final static int NOMBRE_DE_LIGNES = 6;
+
+	public final static int NOMBRE_DE_COLONNES = 7;
 
 	/**
 	 * Le nombre de jetons joués dans la partie
 	 */
-	public static int nombreJetonsJoues;
-
-	/**
-	 * Indicateur permettant de savoir si une colonne est pleine
-	 */
-	// TODO(fait) initialiser l'attribut dans le constructeur
-	public static boolean colonnePleine;
-
-	// TODO (fait) ne pas confondre variable locale et attribut
-	// TODO (fait)respecter les conventions d'écriture
-	/**
-	 * Une colonne que l'utilisateur utilisera pour placer son jeton
-	 */
-	public static int colonne;
-
-	/**
-	 * Ligne permettant de parcourir la colonne et de savoir ou l'on peut écrire
-	 */
-	// TODO (fait)ne pas confondre variable locale et attribut
-	// TODO (fait)respecter les conventions d'écriture
-	// TODO (fait)initialiser l'attribut dans le constructeur
-	public static int ligne;
-
-	// TODO corriger le commentaire
-	/**
-	 * La capacité de la grille de puissance 4
-	 */
-	private final int capaciteGrille;
+	private int nombreJetonsJoues;
 
 	// TODO(fait) écrire un commentaire plus précis
 	/**
 	 * Un tableau ou l'on pourra insérer des jetons .
 	 */
-	public static int[][] grille;
+	private Case[][] grille;
+	
+	private Joueur joueur1;
+	
+	private Joueur joueur2;
+	
 
 	/**
 	 * Crée une nouvelle partie en initialisant la grille et les jetons
 	 */
 	public Puissance4()
-	{	this.ligne=5;
-		this.colonnePleine=true;
-		this.capaciteGrille = CAPACITE_PAR_DEFAUT;
+	{
 		this.nombreJetonsJoues = 0;
-		this.grille = new int[6][7];
-		for (int i = 0; i < this.grille.length; i++)
-		{
-			for (int j = 0; j < this.grille.length; j++)
-			{
-				this.grille[i][j] = 0;
-			}
-		}
-		System.out.println("Les lignes vont de 0 à 5 et les colonnes de 0 à 6");
-	}
+		this.grille = new Case[NOMBRE_DE_LIGNES][NOMBRE_DE_COLONNES];
 
-	
+		for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
+			for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
+				this.grille[numeroDeLigne][numeroDeColonne] = Case.VIDE;
+
+		System.out.println("Les lignes vont de 0 à 5 et les colonnes de 0 à 6");
+		
+		this.joueur1 = new Joueur();
+		this.joueur2 = new Joueur();
+	}
 
 	/**
 	 * Joue l'intégralité d'une partie. Il reste à régler le fait qu'un joueur
@@ -81,13 +58,10 @@ public class Puissance4
 		System.out.println("Partie démarrée");
 		System.out.println("Joueur 1 : Jetons rouge");
 		System.out.println("Joueur 2 : Jetons jaune");
-		while (this.nombreJetonsJoues < this.capaciteGrille)
+		for (int nombreDeTours = 0; nombreDeTours < NOMBRE_DE_COLONNES * NOMBRE_DE_LIGNES; nombreDeTours++)
 		{
-			System.out.println("JOUEUR 1"); // fait jouer le joueur 1
-			Joueur.getColonne();
-			this.nombreJetonsJoues++;
-			System.out.println("JOUEUR 2");
-			Joueur.getColonne();
+			int numeroDeColonneOuJouer = this.joueur1.obtenirColonneOuJouer();
+			/ ...
 			this.nombreJetonsJoues++;
 		}
 
